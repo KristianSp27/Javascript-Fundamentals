e562a47deaa4c994be1b749be11cdf2108fb86ce
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2022-09-28 00:04:01 +0300

    Add files via upload

---
+++ reversedChars.js
@@ -0,0 +1,7 @@
+function reversedChars(charA, charB, charC) {
+  let space = "";
+  let finalCharA = charA + space;
+  let finalCharB = charB + space;
+  let finalCharC = charC + space;
+  console.log(`${finalCharC} ${finalCharB} ${finalCharA} `);
+}
