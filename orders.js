b5827e4fb4867e9871445c1afaca8d2389f4bf9d
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2022-10-08 00:40:27 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,27 @@
+function orders(product, quantity) {
+  let price = 0;
+  switch (product) {
+    case "coffee":
+      price = quantity * 1.5;
+      console.log(price.toFixed(2));
+      break;
+
+    case "water":
+      price = quantity * 1;
+      console.log(price.toFixed(2));
+      break;
+
+    case "coke":
+      price = quantity * 1.4;
+      console.log(price.toFixed(2));
+      break;
+
+    case "snacks":
+      price = quantity * 2;
+      console.log(price.toFixed(2));
+      break;
+
+    default:
+      break;
+  }
+}
