5690d336f92c8bbe9a379d56a83746c692ab2150
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2022-10-09 22:56:18 +0300

    Add files via upload

--- addAndSubstractFunction.js
+++ addAndSubstractFunction.js
@@ -1,8 +1,8 @@
 function addAndSubstract(num1, num2, num3) {
   let sum = num1 + num2;
-}
-
-function substract() {
-  let finalResult = sum - num3;
-  console.log(finalResult);
+  substract(sum, num3);
+  function substract(sum, num3) {
+    let finalResult = sum - num3;
+    console.log(finalResult);
+  }
 }
